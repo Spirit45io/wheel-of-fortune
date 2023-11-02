e80ea8d6f919af8fc576009156efbf0198554ff3
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2023-11-02 17:17:44 +0700

    :construction: Change items

--- lib/data/items.dart
+++ lib/data/items.dart
@@ -1,10 +1,7 @@
 final items = <String>[
-  'Grogu',
-  'Mace Windu',
-  'Obi-Wan Kenobi',
-  'Han Solo',
-  'Luke Skywalker',
-  'Darth Vader',
-  'Yoda',
-  'Ahsoka Tano',
+  'Keychain',
+
+  'T-Shirt',
+  'Book',
+  'Sticker',
 ];
